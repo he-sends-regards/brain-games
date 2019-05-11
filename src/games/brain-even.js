964406e30014d032d34607a94c9616084302224e
postscriptumno <postscriptum.no@gmail.com>
2019-05-11 12:09:23 +0300

    Изменил структуру проекта

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,10 +2,11 @@ import { getRandomInt, builder} from '..';
 
 const rules = 'Answer "yes" if number even otherwise answer "no"';
 
-export const data = () => {
-  const value = getRandomInt(1, 100);
-  const correctAnswer = value % 2 === 0 ? 'yes' : 'no';
-  return [value, correctAnswer];
+const data = () => {
+  const question = getRandomInt(1, 100);
+  const correctAnswer = num => num % 2 === 0 ? 'yes' : 'no';
+  let correct = correctAnswer(question);
+  return [question, correct];
 };
 
 const game = () => builder(rules, data);
--- src/index.js
+++ src/index.js
@@ -3,32 +3,32 @@ import { game } from './games/brain-even';
 
 export const getRandomInt = (min, max) => (Math.floor(Math.random() * (max - min)) + min);
 
-export const builder = (rules, values) => {
+export const builder = (rules, data) => {
+
   console.log('Welcome to the Brain Games!');
+  console.log(rules);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
-  let i = 0;
-  while (i < 3) {
-    game();
-    console.log(`Question ${value}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer) {
-      i += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}". \nLet's try again, ${userName}!`);
-      return;
+  const iter = () => {
+    let i = 0;
+
+    while (i < 3) {
+      let dataValue = data();
+      const question = dataValue[0];
+      const correctAnswer = dataValue[1];
+      console.log(`Question: ${question}`);
+      const answer = readlineSync.question('Your answer: ');
+
+      if (answer === correctAnswer) {
+        i += 1;
+        console.log('Correct!');
+      } else {
+        console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}". \nLet's try again, ${userName}!`);
+        return null;
+      }
     }
+    console.log(`Congratulations, ${userName}!`);
   }
-  console.log(`Congratulations, ${userName}!`);
+  iter();
 }
-
-
-const greeting = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-};
-
-export default greeting;
