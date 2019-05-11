6926e6a38c67010cbe2624a437a993315aede373
postscriptumno <postscriptum.no@gmail.com>
2019-05-11 12:08:45 +0300

    Создал бинарник brain-calc и перестроил структуру проекта

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,41 +1,5 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import game from '../games/brain-calc';
 
-const brainCalc = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('What is the result of the expression?');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-
-  const getRandomInt = (min, max) => (Math.floor(Math.random() * (max - min)) + min);
-
-  const randTask = (num1, num2) => {
-    const sum = (num1, num2) => {
-      console.log('+');
-      return num1 + num2;
-    }
-    const diff = (num1, num2) => {
-      console.log('-');
-      return num1 + num2;
-    }
-    const multi = (num1, num2) => {
-      console.log('*');
-      return num1 * num2;
-    }
-    const operationsArr = [
-      sum(num1, num2),
-      diff(num1, num2),
-      multi(num1, num2)
-    ];
-    return operationsArr[getRandomInt(0, 2)];
-  }
-  let i = 0;
-  while (i < 3) {
-    const num1 = getRandomInt(0, 100);
-    const num2 = getRandomInt(0, 100);
-    console.log(`Question: ${randTask(num1, num2)}`);
-    i += 1;
-  }
-}
-brainCalc();
+game();
---
+++ src/games/brain-calc.js
@@ -0,0 +1,27 @@
+import { getRandomInt, builder} from '..';
+
+const rules = 'What is the result of the expression?';
+
+const data = () => {
+  const num1 = getRandomInt(1, 100);
+  const num2 = getRandomInt(1, 100);
+  const operation = getRandomInt(0, 2);
+  let question;
+  let correctAnswer;
+
+  if (operation === 0) {
+    question = `${num1} + ${num2}`;
+    correctAnswer = num1 + num2;
+  } else if (operation === 1) {
+    question = `${num1} - ${num2}`;
+    correctAnswer = num1 - num2;
+  } else if (operation === 2) {
+    question = `${num1} * ${num2}`;
+    correctAnswer = num1 * num2;
+  }
+  return [question, correctAnswer.toString()];
+};
+
+const game = () => builder(rules, data);
+
+export default game;
