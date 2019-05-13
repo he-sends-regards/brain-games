e625a5e84c3d1a28926817f88bff3c042530e78e
postscriptumno <postscriptum.no@gmail.com>
2019-05-13 15:05:52 +0300

    Создал и написал brain-gcd

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,21 @@
+import { getRandomInt, builder } from '..';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const data = () => {
+  const num1 = getRandomInt(1, 100);
+  const num2 = getRandomInt(1, 100);
+  const question = `${num1} ${num2}`;
+  const gcd = (a, b) => {
+    if (b === 0) {
+      return a;
+    } else return gcd(b, a % b);
+  };
+  const correctAnswer = gcd(num1, num2);
+
+  return [question, correctAnswer.toString()];
+};
+
+const game = () => builder(rules, data);
+
+export default game;
