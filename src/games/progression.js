24a037bb39c425de10c95a3a69ee868f4de12737
postscriptumno <postscriptum.no@gmail.com>
2019-05-20 13:22:01 +0300

    Заменил for на while

--- src/games/progression.js
+++ src/games/progression.js
@@ -10,10 +10,8 @@ const generateGameData = () => {
   const progressionDifference = getRandomInt(1, 10);
   const progression = [];
   let question = `${progression[0]}`;
-  let i = 0;
-  while (i < progressionLength) {
+  for (let i = 0; i < progressionLength; i += 1) {
     progression[i] = firstElement + progressionDifference * i;
-    i += 1;
   }
   const hiddenElementPosition = getRandomInt(0, progressionLength - 1);
   const correctAnswer = progression[hiddenElementPosition];
